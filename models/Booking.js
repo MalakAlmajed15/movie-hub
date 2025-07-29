d8a9155d54a68e943d10cfb5591f7b5e5c20a123
MalakAlmajed15 <malakalmajed15@gmail.com>
2025-07-29 13:15:59 +0300

    creating user schema

---
+++ models/Booking.js
@@ -0,0 +1,11 @@
+// const mongoose = require('mongoose')
+
+// const bookingSchema = new mongoose.Schema({
+//     // movieName: //getting the movie name from the movie schema
+//     date: {
+//         type: Date,
+//         required: true
+//     },
+//     // movieTime: // getting the choosen movie time from the user
+
+// })
